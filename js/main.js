0cc8ca9c161c6c9455060bb968cbc4a313b41f59
Jeremy Niondo <jeremieniondo@gmail.com>
2023-08-01 13:51:27 +0000

    responsive

--- js/main.js
+++ js/main.js
@@ -10,3 +10,17 @@ function btnminus(){
     var  input =  this.parentNode.querySelector('input[type=number]');
     input.stepDown();
 }
+
+
+let menu = document.querySelector('#menu-icon');
+let navlinks = document.querySelector('.nav-links');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlinks.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlinks.classList.remove('active');
+}
